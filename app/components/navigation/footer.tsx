9d13aa7b41d7a688794f6d15d502027a3ecf1d8a
janaiscoding <jana.istrate@gmail.com>
2023-12-06 17:00:27 +0200

    style: add github link and style footer

--- app/components/navigation/footer.tsx
+++ app/components/navigation/footer.tsx
@@ -1,9 +1,19 @@
+import styles from "./navigation.module.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer className="">
-      <div>
-        <FaGithub />
+    <footer className="flex items-center justify-center m-auto">
+      <div className="text-md text-neutral-200 flex gap-2 items-center">
+        Made by
+        <a
+          aria-label="github link for website maker"
+          className={`${styles.footer__text} flex items-center justify-center gap-1 hover:scale-105 transition`}
+          target="_blank"
+          href="https://github.com/janaiscoding"
+        >
+          <FaGithub /> <span> @janaiscoding</span>
+        </a>
       </div>
     </footer>
   );
--- app/components/navigation/navbar.tsx
+++ app/components/navigation/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import styles from "./navbar.module.css";
+import styles from "./navigation.module.css";
 
 const Navbar = () => {
   const fromTop = {
