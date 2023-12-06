ac7ab1e8c05311ab62aa90d36fbf6faf1eeba9c7
janaiscoding <jana.istrate@gmail.com>
2023-12-06 18:45:05 +0200

    docs: start chapters questions structure

---
+++ app/questions/1.get_started/data.ts
@@ -0,0 +1,29 @@
+const bookChapter = "You Don't Know JS Yet: Get Started - 2nd Edition";
+
+const data = [
+  {
+    chapter: 1,
+    quote:
+      "You don't know JS, yet. Neither do I, not fully anyway. None of us do. But we can all start getting to know JS better.",
+    link: "https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/get-started/ch1.md",
+    questions: [],
+  },
+  {
+    chapter: 2,
+    quote: "",
+    link: "https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/get-started/ch2.md",
+    questions: [],
+  },
+  {
+    chapter: 3,
+    quote: "",
+    link: "https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/get-started/ch3.md",
+    questions: [],
+  },
+  {
+    chapter: 4,
+    quote: "",
+    link: "https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/get-started/ch4.md",
+    questions: [],
+  },
+];
