d9a62bcacd9788e0a2f0add13ba6d4b33647cf0a
janaiscoding <jana.istrate@gmail.com>
2023-12-07 18:57:49 +0200

    style: animate text with typewriter-effect

--- app/components/hero-text.tsx
+++ app/components/hero-text.tsx
@@ -1,30 +1,50 @@
 "use client";
 import { motion } from "framer-motion";
-import { fromBottom, fromLeft } from "../utils/animation_variants";
+import { fromBottom } from "../utils/animation_variants";
+import { useState } from "react";
+import TypewriterComponent from "typewriter-effect";
 
 const HeroText = () => {
+  const [buttonText, setButtonText] = useState("I'm ready!");
+
+  const words: string[] = [
+    "the passion for learning!",
+    "the motivation to level up!",
+    "You Don't Know JS Yet!",
+  ];
+
   return (
     <motion.div
       initial="hidden"
       animate="visible"
       variants={fromBottom}
-      className="basis-full flex flex-col gap-6 items-center justify-center"
+      className="basis-full flex flex-col gap-6 items-center justify-center text-center"
     >
-      <h1 className="text-4xl max-w-3xl text-center">
-        A fun, educative JavaScript quiz inspired by the{" "}
-        <span className="text-foreground hover:cursor-pointer hover:text-blue transition">
-          <a
-            target="_blank"
-            href="https://github.com/getify/You-Dont-Know-JS"
-            aria-label="link for the book reference"
-          >
-            You Don&apos;t Know JS
-          </a>
-        </span>{" "}
-        book
-      </h1>
-      <motion.button className="border border-foreground bg-background hover:bg-slate-700 text-foreground px-4 py-2 md:px-6 text-center hover:scale-105 transition">
-        Let&apos;s start!
+      <div className="text-4xl max-w-3xl">
+        A fun, educative quiz inspired by
+        <TypewriterComponent
+          options={{
+            strings: words,
+            autoStart: true,
+            loop: true,
+            wrapperClassName: "",
+            deleteSpeed: 100,
+            delay: 100,
+          }}
+        />
+      </div>
+      <motion.button
+        className="
+      px-4 py-2 md:px-6 
+      border border-foreground 
+      bg-background hover:bg-foreground 
+      text-foreground hover:text-slate-950 
+      transition
+      "
+        onMouseEnter={() => setButtonText("Let's play")}
+        onMouseLeave={() => setButtonText("I'm ready!")}
+      >
+        {buttonText}
       </motion.button>
     </motion.div>
   );
