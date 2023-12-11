486aa36e0563e5dfab007d124ebf013f134affa3
janaiscoding <jana.istrate@gmail.com>
2023-12-11 15:59:53 +0200

    feat: add history under user profiles for personal quiz-stats tracking

--- backend/models/scores.ts
+++ backend/models/scores.ts
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const scoreSchema = new Schema(
   {
-    quiz_id: { type: Schema.Types.ObjectId, ref: "Quiz" },
-    user_id: { type: Schema.Types.ObjectId, ref: "User" },
-    score: { type: Number },
+    quiz_id: { type: Schema.Types.ObjectId, ref: "Quiz", required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    score: {
+      type: Number,
+      required: true,
+      min: [0, "Must be a positive score"],
+    },
   },
   { timestamps: true }
 );
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -6,6 +6,20 @@ const userSchema = new Schema(
     name: { type: String, required: true, minLength: 1 },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    history: [
+      {
+        quiz: { type: Schema.Types.ObjectId, ref: "Quiz" },
+        isComplete: { type: Boolean, default: false },
+        correctQuestions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+        incorrectQuestions: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+        bestScore: {
+          type: Number,
+          required: true,
+          min: [0, "Must be a positive score"],
+          default: 0,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
