62c53270c5ac3320d6b538b2e4e50eb69ef0aead
janaiscoding <jana.istrate@gmail.com>
2023-12-15 19:30:10 +0200

    refactor: allow 1-char answers/titles

--- backend/validators/answer.validator.ts
+++ backend/validators/answer.validator.ts
@@ -3,8 +3,8 @@ import { body } from "express-validator";
 const answer_validator = [
   body("answer", "Answer content must be present")
     .trim()
-    .isLength({ min: 5, max: 5000 })
-    .withMessage("Answer content must be between 5 and 5000 characters"),
+    .isLength({ min: 1, max: 500 })
+    .withMessage("Answer content must be between 1 and 500 characters"),
   body("isCorrect", "isCorrect boolean must be present").isBoolean(),
 ];
 
--- backend/validators/quiz.validator.ts
+++ backend/validators/quiz.validator.ts
@@ -3,8 +3,8 @@ import { body } from "express-validator";
 const quiz_validator = [
   body("title", "Quiz title must be present")
     .trim()
-    .isLength({ min: 5, max: 50 })
-    .withMessage("Quiz title must be between 5 and 50 characters"),
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Quiz title must be between 1 and 50 characters"),
 ];
 
 export { quiz_validator };
