2b49816dcbbe1e59c63a606aaf6e3ae04106ec3e
janaiscoding <jana.istrate@gmail.com>
2023-12-15 21:04:46 +0200

    feat: display error(s) for submitting empty question

--- quiz-cms/app/components/questions/AddQuestionForm.tsx
+++ quiz-cms/app/components/questions/AddQuestionForm.tsx
@@ -9,26 +9,27 @@ const AddQuestionForm = () => {
   const [question, setQuestion] = useState("");
   const quizzes = useContext(QuizzesContext);
   const [parentQuiz, setParentQuiz] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const onSubmitQuestion = (e: FormEvent) => {
     e.preventDefault();
-    if (parentQuiz.includes("Select")) {
-      // add a new error that no quiz was selected
-      return;
+    if (parentQuiz.includes("Select") || !parentQuiz) {
+      setError("Parent quiz is required");
     }
-    if (question && parentQuiz) {
+    if (!question) {
+      setError("Question title is required");
+    } else if (question && parentQuiz && !parentQuiz.includes("Select")) {
       createQuestion(parentQuiz, formatToTemplateLiteral(question), onSuccess);
     }
   };
 
   const onSuccess = (new_question_id: string) => {
+    setError("");
     setQuestion("");
     setParentQuiz("");
-    console.log("redirect..."); // add a visual effect here
-    setTimeout(() => {
-      router.push(`/questions/${new_question_id}`);
-    }, 100);
+
+    router.push(`/questions/${new_question_id}`);
   };
 
   return (
@@ -36,17 +37,32 @@ const AddQuestionForm = () => {
       className="flex flex-col gap-4 px-4 md:min-w-[30rem]"
       onSubmit={(e) => onSubmitQuestion(e)}
     >
+      {/* QUESTION TITLE TEXT AREA */}
       <label className="flex flex-col gap-2 hover:cursor-pointer">
         <span className="md:text-2xl text-indigo-400">Add a new question</span>
         <textarea
           rows={10}
-          onChange={(e) => setQuestion(e.target.value)}
+          onChange={(e) => {
+            if (error) {
+              setError("");
+            }
+            setQuestion(e.target.value);
+          }}
           className="text-neutral-50 bg-neutral-800 border border-indigo-400 p-4"
         ></textarea>
       </label>
+
+      {/* PARENT QUIZ SELECT FIELD */}
       <label className="text-slate-950 flex flex-col gap-2 hover:cursor-pointer">
         <span className="text-indigo-400">Select parent quiz</span>
-        <select onChange={(e) => setParentQuiz(e.target.value)}>
+        <select
+          onChange={(e) => {
+            if (error) {
+              setError("");
+            }
+            setParentQuiz(e.target.value);
+          }}
+        >
           <option>Select parent quiz for this question</option>
           {quizzes.quizzes?.map((quiz) => (
             <option key={quiz._id} value={quiz._id}>
@@ -56,6 +72,10 @@ const AddQuestionForm = () => {
         </select>
       </label>
 
+      {/* DISPLAY ERROR MESSAGE */}
+      {error && <p className="text-xs text-red-200">Error: {error}</p>}
+
+      {/* SUBMIT BUTTON */}
       <button
         type="submit"
         className="self-center flex gap-1 items-center justify-center p-2 text-slate-950 bg-green-400"
