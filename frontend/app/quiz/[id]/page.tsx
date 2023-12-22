74aa0640338e74a54592b57b4bd74a7d2192b1dd
janaiscoding <jana.istrate@gmail.com>
2023-12-22 17:57:41 +0200

    feat: add validation and conditional styles for user answer selection

--- frontend/app/quiz/[id]/page.tsx
+++ frontend/app/quiz/[id]/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 import useQuiz from "@/app/hooks/useQuiz";
-import { useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import Loader from "@/app/ui/Loader";
 import MarkdownWrapper from "@/app/utils/MarkdownWrapper";
 import { formatToMarkdown } from "@/app/utils/stringFormatters";
+import { Answer } from "@/app/utils/types";
 
 const ChapterPage = ({ params }: { params: { id: string } }) => {
   const { isLoading, quiz, questions } = useQuiz(params.id);
-  //const isLoading = true
+
   // Question trigger logic
   const [idx, setIdx] = useState(0);
   const questionRef = useRef<HTMLDivElement>(null);
 
   const incrementIdx = () => {
+    setIsCorrAns(null);
     if (idx + 1 < questions.length && questionRef.current) {
       questionRef.current!.classList.remove("animate__enter");
       questionRef.current.classList.add("animate__exit");
@@ -28,6 +30,7 @@ const ChapterPage = ({ params }: { params: { id: string } }) => {
   };
 
   const decrementIdx = () => {
+    setIsCorrAns(null);
     if (idx > 0 && questionRef.current) {
       questionRef.current.classList.remove("animate__come__from__left");
       questionRef.current.classList.add("animate__go__to__right");
@@ -37,6 +40,16 @@ const ChapterPage = ({ params }: { params: { id: string } }) => {
         questionRef.current!.classList.remove("animate__go__to__right");
         questionRef.current!.classList.add("animate__come__from__left");
       }, 500);
+    }
+  };
+
+  // answer handle logic
+  const [userAns, setUserAns] = useState({} as Answer);
+  const [isCorrectAns, setIsCorrAns] = useState<boolean | null>(null);
+
+  const onCheckAns = () => {
+    if (isCorrectAns === null && userAns.answer) {
+      setIsCorrAns(userAns.isCorrect);
     }
   };
 
@@ -70,15 +83,41 @@ const ChapterPage = ({ params }: { params: { id: string } }) => {
                 content={formatToMarkdown(questions[idx]?.title)}
               />
             </div>
-            <div>
-              {questions[idx].answers.map((ans) => (
-                <div key={ans._id}>{ans.answer}</div>
+            <div className="flex flex-col gap-2">
+              {questions[idx].answers.map((ans, i) => (
+                <label
+                  key={i}
+                  className={`flex gap-2 items-center hover:text-slate-50 hover:cursor-pointer ${
+                    isCorrectAns !== null && ans.isCorrect && "text-green-400"
+                  }`}
+                >
+                  <input
+                    type="checkbox"
+                    value={ans.answer}
+                    onChange={() => {
+                      if (isCorrectAns === null) {
+                        setUserAns(ans);
+                      }
+                    }}
+                    checked={ans.answer === userAns.answer}
+                    className="checkbox checkbox-warning checkbox-sm"
+                  />
+                  <MarkdownWrapper content={ans.answer} />
+                </label>
               ))}
             </div>
+            {isCorrectAns && <p>Correct!</p>}
+            {isCorrectAns === false && <p>Wrong!</p>}
           </div>
           <div className="flex gap-4">
             <button onClick={decrementIdx}>prev</button>
-            <button>submit</button>
+            <button
+              className={"bg-sky-950 p-2 disabled:bg-slate-700"}
+              disabled={isCorrectAns !== null && true}
+              onClick={onCheckAns}
+            >
+              submit
+            </button>
             <button onClick={incrementIdx}>next</button>
           </div>
         </>
