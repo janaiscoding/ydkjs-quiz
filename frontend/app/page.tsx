1736a3090cb7c5a6e69a8292c0a83bef63b29061
janaiscoding <jana.istrate@gmail.com>
2024-03-19 22:30:52 +0100

    Remove loaders from main page, create title component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,37 +1,24 @@
 "use client";
 
 import { useQuizzes } from "./hooks/useQuizzes";
-import React, { useEffect, useState } from "react";
-import { HomepageIntro } from "./components/homepage/HomepageIntro";
-import Loader from "./ui/Loader";
+import PageTitle from "./ui/PageTitle";
 
 export default function Homepage() {
-  const [isShown, setShown] = useState(false);
-  const [isLoading, setLoading] = useState(true);
   const quizzes = useQuizzes();
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+  const title = "Select your quiz!";
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4 min-h-[95vh]">
-      {isLoading && <Loader />}
-
-      {!isLoading && !isShown && <HomepageIntro setShown={setShown} />}
-
-      {!isLoading && (
-        <div className={`${isShown ? "animate__enter" : "hidden"} flex gap-4 flex-col px-16`}>
-          <h1 className="text-xl text-indigo-400">Select your quiz!</h1>
-          {quizzes?.map((quiz, idx) => (
-            <div key={quiz._id}>
-              <a className="text-2xl hover:text-indigo-400" href={`/quiz/${quiz._id}`}>
-                {idx + 1}. {quiz.title} ({quiz.questions.length} questions)
-              </a>
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="flex flex-col items-start p-16 gap-4 min-h-[95vh]">
+      <PageTitle title={title} />
+      {quizzes?.map((quiz, idx) => (
+        <a
+          key={quiz._id}
+          className="text-2xl hover:text-indigo-400"
+          href={`/quiz/${quiz._id}`}
+        >
+          {idx + 1}. {quiz.title} ({quiz.questions.length} questions)
+        </a>
+      ))}
     </div>
   );
 }
---
+++ frontend/app/ui/PageTitle.tsx
@@ -0,0 +1,5 @@
+const PageTitle = ({ title }: { title: string }) => {
+  return <h1 className="text-xl text-indigo-400">{title}</h1>;
+};
+
+export default PageTitle;
